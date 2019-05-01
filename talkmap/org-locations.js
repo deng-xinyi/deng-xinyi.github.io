b4f9a14cd6093a9411823e01d8433eaaf54bfedf
deng-xinyi <43009981+deng-xinyi@users.noreply.github.com>
2019-04-30 21:05:38 -0400

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -18,7 +18,32 @@ var addressPoints = [
     "Milan, Italy. Invited session. International Conference of the IEEE Engineering In Medicine and Biology Society. August 2015", 
     45.4667971, 
     9.1904984
-  ],  
+  ], 
+  [
+    "Wuhan, China. Seminar. School of Mathematics and Statistics, Wuhan University. January 2019", 
+    30.5928, 
+    114.3055
+  ],
+  [
+    "Beijing, China. Seminar. Department of Statistics, Beijing University of Technology. January 2019", 
+    39.9042, 
+    116.4074
+  ],
+  [
+    "Providence, RI. Statistical Neuroscience Lunch Seminar. Brown Institute for Brain Science, Brown University. October 2015", 
+    41.8240, 
+    -71.4128
+  ], 
+  [
+    "Pittsburgh, PA. Young Investigator Lecture. 7th International Workshop on Statistical Analysis of Neuronal Data (SAND7). May 2015", 
+    40.4406, 
+    -79.9959
+  ],    
+  [
+    "Washington, DC. Seminar. Department of Mathematics and Statistics, American University. October 2014", 
+    38.9072, 
+    -77.0369
+  ],      
   [
     "Snowbird, UT. Minisymposium. Society for Industrial and Applied Mathematics Conference on Applications of Dynamical Systems (SIAM DS). May 2015",
     40.3452,
