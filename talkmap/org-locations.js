cd55e796ecd958769bf524033c5cd259b9a157b4
deng-xinyi <43009981+deng-xinyi@users.noreply.github.com>
2019-04-30 22:53:30 -0400

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,7 +5,7 @@ var addressPoints = [
     14.4378
   ],
   [
-    "Quebec city, Canada. Poster. Organization for Computational Neurosciences Annual Meeting. July 2014",
+    "Quebec City, Canada. Poster. Organization for Computational Neurosciences Annual Meeting. July 2014",
     46.8139,
     -71.2080
   ],
@@ -43,7 +43,22 @@ var addressPoints = [
     "Washington, DC. Seminar. Department of Mathematics and Statistics, American University. October 2014", 
     38.9072, 
     -77.0369
-  ],      
+  ], 
+  [
+    "San Diego, CA. Poster. Society for Neuroscience Annual Meeting (SfN). November 2016", 
+    32.7157, 
+    -117.1611
+  ],
+  [
+    "Chicago, IL. Poster. Society for Neuroscience Annual Meeting (SfN). October 2015", 
+    41.8781, 
+    -87.6298
+  ], 
+  [
+    "Cambridge, MA. Poster. Rhythmic Dynamics and Cognition Conference, MIT. June 2013", 
+    42.3736, 
+    -71.1097
+  ], 
   [
     "Snowbird, UT. Minisymposium. Society for Industrial and Applied Mathematics Conference on Applications of Dynamical Systems (SIAM DS). May 2015",
     40.3452,
